12a0d4eadecf23c8391fb5e8316f82be69fd97d7
JUNlOR <bloody.junior@gmail.com>
2015-08-20 16:49:57 +0300

    add task complete status

--- src/js/addNewTask.js
+++ src/js/addNewTask.js
@@ -2,15 +2,18 @@
  * Created by Junior on 19.08.2015.
  */
 
-(function(document){ "use strict";
+(function (document) {
+    "use strict";
 
-    var taskList = ["task 1", "task 2", "task 3"];
+    var taskList = [{"id": 1, "taskbody": "task 1", "status": "done"},
+        {"id": 2, "taskbody": "task 2", "status": "in progress"},
+        {"id": 3, "taskbody": "task 3", "status": "done"}];
     var taskListID = document.getElementById("taskListID");
 
     function printTask() {
         for (var i = 0, n = taskList.length; i < n; i++) {
             var li = document.createElement("li");
-            li.innerHTML = taskList[i];
+            li.innerHTML = taskList[i].taskbody + " --> status - " + taskList[i].status;
             taskListID.appendChild(li);
         }
     }
