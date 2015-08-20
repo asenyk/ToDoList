721f41503078e6830c18aeb06b3dcf2c0f158039
JUNlOR <bloody.junior@gmail.com>
2015-08-20 18:47:34 +0300

    add checkboxes with status

--- src/js/addNewTask.js
+++ src/js/addNewTask.js
@@ -5,16 +5,24 @@
 (function (document) {
     "use strict";
 
-    var taskList = [{"id": 1, "taskbody": "task 1", "status": "done"},
-        {"id": 2, "taskbody": "task 2", "status": "in progress"},
-        {"id": 3, "taskbody": "task 3", "status": "done"}];
+    var taskList = [{"id": 1, "taskbody": "task 1", "done": true},
+        {"id": 2, "taskbody": "task 2", "done": false},
+        {"id": 3, "taskbody": "task 3", "done": true}];
     var taskListID = document.getElementById("taskListID");
 
     function printTask() {
         for (var i = 0, n = taskList.length; i < n; i++) {
             var li = document.createElement("li");
+            var input = document.createElement("input");
+            input.setAttribute("type", "checkbox");
+            if (taskList[i].done === true){
+                input.checked = true;
+            }
+            li.className = "list-group-item";
+            input.className = "statuscheckboxStatus";
             li.innerHTML = taskList[i].taskbody + " --> status - " + taskList[i].status;
             taskListID.appendChild(li);
+            li.insertBefore(input, li.firstChild);
         }
     }
 
