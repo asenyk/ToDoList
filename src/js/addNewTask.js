e4ce02b91f1e31e4c20ff502d0de90fb4b6b3849
JUNlOR <bloody.junior@gmail.com>
2015-08-20 16:30:40 +0300

    js optimization

--- src/js/addNewTask.js
+++ src/js/addNewTask.js
@@ -2,31 +2,19 @@
  * Created by Junior on 19.08.2015.
  */
 
-var taskList = ["task 1", "task 2", "task 3"];
+(function(document){ "use strict";
 
-var printTask = function (taskList, taskListID, document) {
-    for (i = 0, n = taskList.length; i < n; i++) {
-        var li = document.createElement("li");
-        li.innerHTML = taskList[i];
-        taskListID.appendChild(li);
-    }
-}
+    var taskList = ["task 1", "task 2", "task 3"];
+    var taskListID = document.getElementById("taskListID");
 
-var onLoadWindow = function(window) {
-    var result;
-    if (window.addEventListener){
-        result = function(callback) {
-            window.addEventListener("load", callback, false);
-        };
-    } else if (window.attachEvent){
-        result = function(callback) {
-            window.attachEvent("onload", callback);
-        };
-    } else {
-        result = function() {};
+    function printTask() {
+        for (var i = 0, n = taskList.length; i < n; i++) {
+            var li = document.createElement("li");
+            li.innerHTML = taskList[i];
+            taskListID.appendChild(li);
+        }
     }
-    return result;
-}(window);
-onLoadWindow(function() {
-    printTask(taskList, document.getElementById("taskListID"), document);
-});
+
+    document.addEventListener("DOMContentLoaded", printTask);
+
+}(document));
